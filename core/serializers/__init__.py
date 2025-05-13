1e06939f1e32b9e8bb372ac4daee7a724ed9a2cd
Lucas-EstudanteIFC <alunolucasmartins62@gmail.com>
2025-05-13 08:14:39 -0300

    feat: criação de dois serializadores para Livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,5 +2,5 @@ from .user import UserSerializer
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
-from .livro import LivroSerializer
+from .livro import LivroListRetrieveSerializer, LivroSerializer 
 
